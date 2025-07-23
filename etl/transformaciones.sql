a114c8bbe433596dc1f6f10044922b65ff69f887
Ysaac <ysaacsalvatierra99@gmail.com>
2025-07-23 15:15:07 -0300

    Update transformaciones.sql

--- etl/transformaciones.sql
+++ etl/transformaciones.sql
@@ -7,19 +7,47 @@
 SELECT * FROM train;
 
 -- 2. Estandarizar formato de fecha (si fuera necesario)
--- SELECT [Order Date], CAST([Order Date] AS DATE) FROM train;
--- UPDATE train SET [Order Date] = CAST([Order Date] AS DATE);
+SELECT [Order Date], [Ship Date]
+FROM dbo.DatosTrainRaw;
 
--- 3. Reemplazar valores nulos en columnas clave
--- SELECT * FROM train WHERE [Customer Name] IS NULL;
--- UPDATE train SET [Customer Name] = 'Desconocido' WHERE [Customer Name] IS NULL;
+UPDATE dbo.DatosTrainRaw
+ SET [Order Date] = CAST([Order Date] AS DATE),
+     [Ship Date] = CAST([Ship Date] AS DATE);
+
+-- 3. Reemplazar valores nulos en columnas clave (Sé que hay maneras de hacerla con scripts, pero me falta estudiarlo para saber hacerlo mejor)
+SELECT *
+FROM dbo.DatosTrainRaw
+WHERE 
+    [Row ID] IS NULL OR
+    [Order ID] IS NULL OR
+    [Order Date] IS NULL OR
+    [Ship Date] IS NULL OR
+    [Ship Mode] IS NULL OR
+    [Customer ID] IS NULL OR
+    [Customer Name] IS NULL OR
+    [Segment] IS NULL OR
+    [Country] IS NULL OR
+    [City] IS NULL OR
+    [State] IS NULL OR
+    [Postal Code] IS NULL OR
+    [Region] IS NULL OR
+    [Product ID] IS NULL OR
+    [Category] IS NULL OR
+    [Sub-Category] IS NULL OR
+    [Product Name] IS NULL OR
+    [Sales] IS NULL
 
 -- 4. Eliminar filas duplicadas (si aplica)
--- WITH CTE_Duplicates AS (
---     SELECT *, ROW_NUMBER() OVER (PARTITION BY [Order ID], [Product ID] ORDER BY [Row ID]) AS fila
---     FROM train
--- )
--- DELETE FROM CTE_Duplicates WHERE fila > 1;
+WITH FilasDuplicadas AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY [Order ID], [Product ID]
+               ORDER BY [Row ID]
+           ) AS fila
+    FROM dbo.DatosTrainRaw
+)
+DELETE FROM CTE_Duplicados
+WHERE fila > 1;
 
 -- 5. Crear nuevas columnas derivadas (ejemplo: año de orden)
 -- SELECT [Order Date], YEAR([Order Date]) AS AñoOrden FROM train;
