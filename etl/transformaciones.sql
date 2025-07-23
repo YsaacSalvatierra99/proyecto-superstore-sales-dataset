224d75520d1380460c05de8866de60dfc2e1b4e2
Ysaac <ysaacsalvatierra99@gmail.com>
2025-07-23 19:26:31 -0300

    Update transformaciones.sql

--- etl/transformaciones.sql
+++ etl/transformaciones.sql
@@ -1,3 +1,45 @@
+/*CREACION DE TABLA
+
+-- 1. Primero lo abrí en Excel para separar los datos ya que estaban todos juntos en una misma celda, y necesitaba que esten separados por su delimitador de ",".
+Una vez abierto el Excel, fui a Datos/Texto en columnas/Delimitados/Coma.
+
+Ahora me faltaba crear una tabla e importar el data set a la tabla de SQL server.
+
+-- 2. Creacion de tabla tipo RAW(Elegi crear primero una tabla tipo RAW ya que el data set no era muy grande y tenia problemas en la importacion y no sabia si era por el formato de alguna columna o elementos que se me escapaban a entenderlo)
+Use Principal*/
+
+CREATE TABLE dbo.DatosTrainRaw (
+    [Row ID] VARCHAR(MAX),
+    [Order ID] VARCHAR(MAX),
+    [Order Date] VARCHAR(MAX),
+    [Ship Date] VARCHAR(MAX),
+    [Ship Mode] VARCHAR(MAX),
+    [Customer ID] VARCHAR(MAX),
+    [Customer Name] VARCHAR(MAX),
+    [Segment] VARCHAR(MAX),
+    [Country] VARCHAR(MAX),
+    [City] VARCHAR(MAX),
+    [State] VARCHAR(MAX),
+    [Postal Code] VARCHAR(MAX),
+    [Region] VARCHAR(MAX),
+    [Product ID] VARCHAR(MAX),
+    [Category] VARCHAR(MAX),
+    [Sub-Category] VARCHAR(MAX),
+    [Product Name] VARCHAR(MAX),
+    [Sales] VARCHAR(MAX)
+);
+
+/* Luego Ingrese el data set superstore-sale-dataset que su nombre predeterminado al descargarlo era train.csv  usando BULK INSERT, por temas de compatibilidad lo mantuve asi, para ver si fue ese el error. */
+
+BULK INSERT dbo.DatosTrainRaw
+FROM 'C:\TABLAS DE DATOS\train.csv'
+WITH (
+    FIELDTERMINATOR = ';',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+/* Una vez cree la tabla tipo RAW e ingresé la base de datos. Ahora modifiqué el tipo de dato de las columnas y tambien de los datos para que coincidan, la tabla de diseño se encuentra en el README. */
 /*
     Limpieza de datos en SQL - Superstore Dataset
     Habilidades aplicadas: SELECT, UPDATE, CAST, CONVERT, ISNULL, CASE, GROUP BY
