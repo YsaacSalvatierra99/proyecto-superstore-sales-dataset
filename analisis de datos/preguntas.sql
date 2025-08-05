961351c5ab3a321d9a3e4b759041e2be89ed7ad3
Ysaac <ysaacsalvatierra99@gmail.com>
2025-08-05 16:40:53 -0300

    Create preguntas.sql

---
+++ analisis de datos/preguntas.sql
@@ -0,0 +1,31 @@
+/*
+
+VENTAS
+--1. Zonas con mayores ventas por Año-Mes (YearMonth)
+--2. Zonas con mayores ventas por Trimestre (Quarter)
+--3. Zonas con mayores ventas por Semestre (Semester)
+-- Sacar una conclusion de si hay alguna estacionalidad
+
+
+PRODUCTOS
+--1. Productos mas vendidos separados por categoria 
+--2. Productos con entregas más demoradas (DeliveryTime alto)
+--3. Productos vendidos solo 1 vez en todo el histórico
+
+
+TIEMPOS DE ENTREGA
+--1. DeliveryTime promedio por tipo de envío (Ship Mode)
+--2. Órdenes con mayor DeliveryTime
+
+
+CLIENTES
+
+--1. Que tipo de cliente compra mas segun tiempo, zona y producto
+--2. ¿Qué tipo de cliente (Segment) compra más según trimestre/zona?
+--3. Revisar la demora de entrega en cada caso para ver si tiene relacion
+
+
+ALERTAS
+
+--1. Ventas con DeliveryTime inusualmente alto (>15 días)
+*-
