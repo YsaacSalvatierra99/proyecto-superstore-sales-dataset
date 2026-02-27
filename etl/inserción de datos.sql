f9892725f9e5f9c00aa3e871997ab72f306a237a
Ysaac <ysaacsalvatierra99@gmail.com>
2026-02-27 16:26:18 -0300

    Update inserción de datos.sql

--- etl/inserción de datos.sql
+++ etl/inserción de datos.sql
@@ -51,3 +51,36 @@ SELECT
     [Customer ID],
     [Product ID]
 FROM DatosTrainRaw;
+
+/*Inserción de los datos de la tabla Geografia.
+  La diferencia con las demas inserciones es que esta te normaliza los espacios en blancos del principio y final,
+  tambien las mayusculas, solo la primera letra de cada palabra.
+
+  Cuando pueda deberia usar este metodo para las demas inserciones, vaciando las tablas y colocandolas de nuevo 
+  pero con esta version.
+*/
+
+
+INSERT INTO Geografia (Postal_Code, City, State, Country, Region)
+SELECT 
+    TRIM(CAST([Postal Code] AS VARCHAR(20))), 
+    UPPER(TRIM(MAX(City))), 
+    UPPER(TRIM(MAX(State))), 
+    UPPER(TRIM(MAX(Country))), 
+    UPPER(TRIM(MAX(Region)))
+FROM DatosTrainRaw
+WHERE [Postal Code] IS NOT NULL
+GROUP BY [Postal Code];
+
+/* Inserción de los datos en la tabla Tiempo*/
+
+INSERT INTO Tiempo (Order_Date, Year, Month, Month_Name, Quarter, Semester)
+SELECT DISTINCT 
+    [Order Date], 
+    Year, 
+    Month, 
+    UPPER(TRIM(MonthName)), 
+    Quarter, 
+    UPPER(TRIM(Semester))
+FROM DatosTrainRaw
+WHERE [Order Date] IS NOT NULL;
