4da277ac0a9551ffb7ca1eb48cbce2b808ea0a74
Ysaac <ysaacsalvatierra99@gmail.com>
2026-03-04 13:06:42 -0300

    Update 02_analisis_diagnostico.sql

--- analisis de datos/02_analisis_diagnostico.sql
+++ analisis de datos/02_analisis_diagnostico.sql
@@ -29,6 +29,30 @@ ORDER BY
 	month;
 
 -- Dependencia del cliente top.
+WITH ventas_trimestrales AS (
+	SELECT 
+		T.year,
+		T.quarter,
+		SUM(V.sales) AS total_sales
+	FROM Ventas V
+	JOIN Tiempo T
+		ON V.order_date = T.order_date
+	GROUP BY 
+		T.year,
+		T.quarter
+)
+
+SELECT 
+	year,
+	quarter,
+	total_sales,
+	LAG(total_sales) OVER (ORDER BY year,quarter) AS ventas_trimestre_anterior,
+	((total_sales - LAG(total_sales) OVER (ORDER BY year,quarter))* 100.00 
+	/ LAG(total_sales) OVER (ORDER BY year,quarter)) AS Crecimiento_QoQ_pct
+FROM ventas_trimestrales
+ORDER BY
+	year,
+	quarter;
 
 -- Impacto DeliveryTime en ventas.
 -- Productos con baja rotación.
