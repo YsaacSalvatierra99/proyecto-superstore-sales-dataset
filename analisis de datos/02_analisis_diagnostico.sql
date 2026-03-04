e586e091fdfc92f5efa3cfd32824ad9f97b12854
Ysaac <ysaacsalvatierra99@gmail.com>
2026-03-04 12:27:34 -0300

    Update 02_analisis_diagnostico.sql

--- analisis de datos/02_analisis_diagnostico.sql
+++ analisis de datos/02_analisis_diagnostico.sql
@@ -1,4 +1,34 @@
 -- Crecimiento MoM / QoQ.
+SELECT * FROM Ventas;
+SELECT * FROM Tiempo;
+
+
+WITH ventas_mensuales AS (
+	SELECT 
+		T.year,
+		T.month,
+		SUM(V.sales) AS total_sales
+	FROM Ventas V
+	JOIN Tiempo T
+		ON V.order_date = T.order_date
+	GROUP BY 
+		T.year,
+		T.month
+)
+
+SELECT
+	year,
+	month,
+	total_sales,
+	LAG(total_sales) OVER (ORDER BY year, month) AS ventas_mes_anterior,
+	((total_sales - LAG(total_sales) OVER (ORDER BY year, month)) *100.0
+	/ LAG(total_sales) OVER (ORDER BY year, month)) AS crecimiento_MoM_pct
+FROM ventas_mensuales
+ORDER BY
+	year,
+	month;
+
 -- Dependencia del cliente top.
+
 -- Impacto DeliveryTime en ventas.
 -- Productos con baja rotación.
