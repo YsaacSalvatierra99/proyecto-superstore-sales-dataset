3bb6efd87066c5651374d4c24b376b03c5ad2cc0
Ysaac <ysaacsalvatierra99@gmail.com>
2026-03-14 19:27:45 -0300

    Update 02_analisis_diagnostico.sql

--- analisis de datos/02_analisis_diagnostico.sql
+++ analisis de datos/02_analisis_diagnostico.sql
@@ -1,3 +1,136 @@
+-- Ventas por mes
+SELECT 
+    T.month_name, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.month_name
+ORDER BY MIN(V.order_date);
+
+-- Ventas por trimestre
+SELECT 
+    T.quarter, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.quarter;
+
+-- Ventas por año
+SELECT 
+    T.year, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.year
+ORDER BY T.year;
+
+-- Que meses venden mas?
+SELECT TOP 5 
+    T.month_name, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.month_name
+ORDER BY total_ventas DESC;
+
+-- Que trimestre genera mas ingresos?
+SELECT TOP 1 
+    T.quarter, 
+    SUM(V.sales) AS total_ingresos
+FROM dbo.Ventas V
+INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.quarter
+ORDER BY total_ingresos DESC;
+-----------------------------------------------------------------------------
+-- Ventas por categoria.
+SELECT 
+    P.category, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Productos P ON V.product_id = P.product_id
+GROUP BY P.category
+ORDER BY total_ventas DESC;
+
+-- Ventas por subcategoria.
+SELECT 
+    P.sub_category, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Productos P ON V.product_id = P.product_id
+GROUP BY P.sub_category
+ORDER BY total_ventas DESC;
+
+-- Ventas por producto.
+SELECT 
+    P.product_name, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Productos P ON V.product_id = P.product_id
+GROUP BY P.product_name
+ORDER BY total_ventas DESC;
+
+-- Top 10 productos mas vendidos por categoria.
+SELECT TOP 10 
+    P.category,
+    P.product_name, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+INNER JOIN dbo.Productos P ON V.product_id = P.product_id
+GROUP BY P.category, P.product_name
+ORDER BY total_ventas DESC;
+
+----------------------------------------------------------------------
+-- Ventas mensuales por categoria.
+SELECT 
+    T.year,
+    T.month_name, 
+    P.category, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+JOIN dbo.Productos P ON V.product_id = P.product_id
+JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.year, T.month_name, P.category
+ORDER BY T.year, MIN(V.order_date), P.category;
+
+-- Ventas trimestrales por categoria.
+SELECT 
+    T.year,
+    T.quarter, 
+    P.category, 
+    SUM(V.sales) AS total_ventas
+FROM dbo.Ventas V
+JOIN dbo.Productos P ON V.product_id = P.product_id
+JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.year, T.quarter, P.category
+ORDER BY T.year, T.quarter, P.category;
+
+-- Ventas Q vs MPAA por categoria.
+SELECT 
+    T.year,
+    T.quarter,
+    P.category,
+    SUM(V.sales) AS ventas_actuales,
+    LAG(SUM(V.sales), 4) OVER (PARTITION BY P.category ORDER BY T.year, T.quarter) AS ventas_mismo_trimestre_anio_anterior
+FROM dbo.Ventas V
+JOIN dbo.Productos P ON V.product_id = P.product_id
+JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.year, T.quarter, P.category;
+
+-- Ventas Q vs Q por categoria.
+SELECT 
+    T.year,
+    T.quarter,
+    P.category,
+    SUM(V.sales) AS ventas_actuales,
+    LAG(SUM(V.sales)) OVER (PARTITION BY P.category ORDER BY T.year, T.quarter) AS ventas_trimestre_anterior
+FROM dbo.Ventas V
+JOIN dbo.Productos P ON V.product_id = P.product_id
+JOIN dbo.Tiempo T ON V.order_date = T.order_date
+GROUP BY T.year, T.quarter, P.category;
+
+
+------------------------------------------------------------------
+
 -- Crecimiento MoM.
 SELECT * FROM Ventas;
 SELECT * FROM Tiempo;
