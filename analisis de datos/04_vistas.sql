ed9e29306d2f6bb43d79d1e456bb692fa821c481
Ysaac <ysaacsalvatierra99@gmail.com>
2026-03-14 22:02:41 -0300

    Update 04_vistas.sql

--- analisis de datos/04_vistas.sql
+++ analisis de datos/04_vistas.sql
@@ -1,1 +1,25 @@
+   --Vista general para usar en Power BI--
 
+CREATE VIEW vista_general AS
+SELECT
+    V.order_id,
+    V.sales,
+    C.customer_name,
+    C.segment,
+    P.product_name,
+    P.category,
+    U.city,
+    U.state,
+    U.region,
+    D.year,
+    D.month_name,
+    D.quarter
+FROM ventas V
+JOIN clientes C
+ON V.customer_id = C.customer_id
+JOIN productos P
+ON V.product_id = P.product_id
+JOIN Geografia U
+ON V.postal_code = U.postal_code
+JOIN Tiempo D
+ON V.order_date = D.order_date;
