1accfbab5338484f8a4229bd709d8064643e34b2
Ysaac <ysaacsalvatierra99@gmail.com>
2026-03-14 22:00:46 -0300

    Update 01_analisis_descriptivo.sql

--- analisis de datos/01_analisis_descriptivo.sql
+++ analisis de datos/01_analisis_descriptivo.sql
@@ -1,23 +1,24 @@
---EDA basico.
 
---Cuantas transacciones unicas hay registradas.
+                 --EDA BÁSICO--
+
+--Ventas únicas hay registradas--
 SELECT COUNT(*) FROM ventas;
 
--- Cuantos clientes unicos hay registrados.
+-- Clientes unicos hay registrados--
 SELECT COUNT(*) FROM clientes;
 
--- Cuantos productos unicos hay registrados.
+-- Productos unicos hay registrados--
 SELECT COUNT(*) FROM productos;
 
--- Rango de fechas que abarca la base de datos a analizar.
+-- Rango de fechas que abarca la base de datos a analizar--
 SELECT MIN(order_date), MAX(order_date)
 FROM ventas;
 
--- Ventas totales (Dentro del rango de fecha del BBDD).
+-- Ventas totales (Dentro del rango de fecha del BBDD)--
 SELECT SUM(sales) total_sales
 FROM ventas;
 
--- Ventas por mes
+-- Ventas por mes--
 SELECT 
     T.month_name, 
     SUM(V.sales) AS total_ventas
@@ -26,7 +27,7 @@ INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
 GROUP BY T.month_name
 ORDER BY MIN(V.order_date);
 
--- Ventas por trimestre
+-- Ventas por trimestre--
 SELECT 
     T.quarter, 
     SUM(V.sales) AS total_ventas
@@ -34,7 +35,7 @@ FROM dbo.Ventas V
 INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
 GROUP BY T.quarter;
 
--- Ventas por año
+-- Ventas por año--
 SELECT 
     T.year, 
     SUM(V.sales) AS total_ventas
@@ -43,7 +44,7 @@ INNER JOIN dbo.Tiempo T ON V.order_date = T.order_date
 GROUP BY T.year
 ORDER BY T.year;
 
---Ventas por Categoría.
+--Ventas por Categoría--
 SELECT 
     P.category, 
     SUM(V.sales) AS total_ventas
@@ -52,7 +53,7 @@ INNER JOIN dbo.Productos P ON V.product_id = P.product_id
 GROUP BY P.category
 ORDER BY total_ventas DESC;
 
--- Ventas por subcategoria.
+-- Ventas por subcategoria--
 SELECT 
     P.sub_category, 
     SUM(V.sales) AS total_ventas
@@ -61,7 +62,7 @@ INNER JOIN dbo.Productos P ON V.product_id = P.product_id
 GROUP BY P.sub_category
 ORDER BY total_ventas DESC;
 
--- Ventas por producto.
+-- Ventas por producto--
 SELECT 
     P.product_name, 
     SUM(V.sales) AS total_ventas
@@ -70,7 +71,7 @@ INNER JOIN dbo.Productos P ON V.product_id = P.product_id
 GROUP BY P.product_name
 ORDER BY total_ventas DESC;
 
---Ventas por Segmento.
+--Ventas por Segmento--
 SELECT
 	C.segment,
 	SUM(V.sales) AS total_sales,
